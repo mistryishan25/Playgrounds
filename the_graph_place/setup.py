2aef70efaa3824c1135c176392ac054a861f534f
mistryishan25 <mistryishan25@gmail.com>
2022-04-02 22:32:38 +0530

    Added the cookiecutter structure

---
+++ the_graph_place/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='All the Graph stuff I've ever touched!',
+    author='Ishan Mistry',
+    license='MIT',
+)
